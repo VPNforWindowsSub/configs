3c36631bb65a203e68eb0d43b80d6a01a01b72c0
hoeci <164579566+hoeci@users.noreply.github.com>
2025-08-07 04:22:45 +0330

    Add files via upload

--- utils/speedtest/speedtest2_yamlc.sh
+++ utils/speedtest/speedtest2_yamlc.sh
@@ -6,7 +6,6 @@ echo "Downloading LiteSpeedTest binary..."
 wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
 gzip -d lite-linux-amd64.gz
 
-
 echo "Starting Mihomo with local multi-provider config..."
 chmod +x ./clash
 sudo pkill -f clash
@@ -15,4 +14,4 @@ sleep 5
 
 echo "Running LiteSpeedTest with local config and local proxy list..."
 chmod +x ./lite-linux-amd64
-sudo nohup ./lite-linux-amd64 --config ./utils/speedtest/lite_config_yaml.json --test ./sub/sub_merge_yaml.yml > speedtest.log 2>&1 &
+sudo ./lite-linux-amd64 --config ./utils/speedtest/lite_config_yaml.json --test ./sub/sub_merge_yaml.yml 2>&1 | tee speedtest.log
