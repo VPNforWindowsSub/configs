1e4b37027ebf8dac4e140f45c79df3a9e721420c
hoeci <164579566+hoeci@users.noreply.github.com>
2025-08-08 01:44:42 +0330

    Add files via upload

--- utils/speedtest/speedtest2_yamlc.sh
+++ utils/speedtest/speedtest2_yamlc.sh
@@ -1,12 +1,15 @@
-echo "Downloading Mihomo v1.19.12 binary..."
-wget -O mihomo.gz https://github.com/MetaCubeX/mihomo/releases/download/v1.19.12/mihomo-linux-amd64-v1.19.12.gz
-gunzip -c mihomo.gz > clash
+# Clean up any previous binaries
+rm -f mihomo.gz clash lite-linux-amd64.gz lite-linux-amd64
 
-echo "Starting Mihomo with the dedicated test configuration..."
-chmod +x ./clash
-sudo pkill -f clash
-./clash -f ./utils/speedtest/clash_test_config.yml &
-sleep 10
+echo "Downloading SingTools binary..."
+wget -O singtools_linux32.tar.gz https://github.com/Kdwkakcs/singtools/releases/download/vv0.2.0/singtools_linux32.tar.gz
 
-echo "Running API-based speed test using Python script..."
-python3 ./utils/speedtest/api_speedtest.py 2>&1 | tee speedtest.log
+echo "Extracting SingTools..."
+tar -zxvf singtools_linux32.tar.gz
+chmod +x ./singtools
+
+echo "Running SingTools speed test..."
+# The -i flag specifies the input file with the proxy nodes.
+# sub_merge_yaml.yml is the file containing the collected proxies in a format SingTools can likely parse.
+# The default output is out.json, which is what the subsequent output.py script expects.
+./singtools test -i ./sub/sub_merge_yaml.yml -c ./utils/speedtest/singtools_config.json 2>&1 | tee speedtest.log
