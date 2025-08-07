2b591b5d4f5e39785f09400370b93b75ba8cfa76
hoeci <164579566+hoeci@users.noreply.github.com>
2025-08-07 04:16:40 +0330

    Add files via upload

--- utils/speedtest/speedtest2_yamlc.sh
+++ utils/speedtest/speedtest2_yamlc.sh
@@ -1,15 +1,11 @@
 echo "Downloading Mihomo v1.19.12 binary..."
 wget -O mihomo.gz https://github.com/MetaCubeX/mihomo/releases/download/v1.19.12/mihomo-linux-amd64-v1.19.12.gz
-
 gunzip -c mihomo.gz > clash
 
 echo "Downloading LiteSpeedTest binary..."
 wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
 gzip -d lite-linux-amd64.gz
 
-echo "Setting up ProxyChains using local config file..."
-sudo apt-get install proxychains -y
-sudo mv -f ./utils/speedtest/proxychains.conf /etc/proxychains.conf
 
 echo "Starting Mihomo with local multi-provider config..."
 chmod +x ./clash
@@ -19,4 +15,4 @@ sleep 5
 
 echo "Running LiteSpeedTest with local config and local proxy list..."
 chmod +x ./lite-linux-amd64
-sudo nohup proxychains ./lite-linux-amd64 --config ./utils/speedtest/lite_config_yaml.json --test ./sub/sub_merge_yaml.yml > speedtest.log 2>&1 &
+sudo nohup ./lite-linux-amd64 --config ./utils/speedtest/lite_config_yaml.json --test ./sub/sub_merge_yaml.yml > speedtest.log 2>&1 &
